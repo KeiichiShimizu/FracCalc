9469f0c515f1ad2fe500fec63e3960dcf0a8b674
KeiichiShimizu <keiichi.tkb@hotmail.co.jp>
2013-10-27 18:16:14 +0900

    COINSFracLabelの追加

---
+++ FracCalc/COINSFracLabel.h
@@ -0,0 +1,20 @@
+//
+//  COINSFracLabel.h
+//  FracCalc
+//
+//  Created by Shimizu Keiichi on 2013/10/25.
+//  Copyright (c) 2013年 COINS Project AID. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface COINSFracLabel : UILabel
+
+@property UILabel *leftLabel;
+
+-(id)update:(char) c;
+
+
+
+@end
+
---
+++ FracCalc/COINSFracLabel.m
@@ -0,0 +1,35 @@
+//
+//  COINSFracLabel.m
+//  FracCalc
+//
+//  Created by Shimizu Keiichi on 2013/10/25.
+//  Copyright (c) 2013年 COINS Project AID. All rights reserved.
+//
+
+#import "COINSFracLabel.h"
+
+@implementation COINSFracLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+-(id)update:(char)c{
+    
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
